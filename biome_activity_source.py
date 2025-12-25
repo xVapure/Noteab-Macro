d9057c59806c7cd08cb6635c741523ebec208bad
xVapure <work.vapure@gmail.com>
2025-12-25 20:44:41 +0700

    Update biome_activity_source.py

--- biome_activity_source.py
+++ biome_activity_source.py
@@ -12,7 +12,7 @@ import difflib
 import json, requests, time, os, threading, re, webbrowser, random, keyboard, pyautogui, pytesseract, autoit, psutil, \
     locale, win32gui, win32process, win32con, ctypes, queue, mouse, sys
 
-current_ver = "v2.0.4"
+current_ver = "v2.0.4-hotfix"
 
 def apply_fast_flags(version=None, force=False):
     config_paths = [
@@ -5999,6 +5999,7 @@ class BiomePresence():
             time.sleep(0.65)
 
             self.autoit_hold_left_click(merchant_dialogue_box[0], merchant_dialogue_box[1], holdTime=4250)
+            time.sleep(1.67) # 67 tuff
             self.Global_MouseClick(merchant_open_button[0], merchant_open_button[1], click=3)
 
             for _ in range(6):
