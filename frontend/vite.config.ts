1a839ad53d03e480da7581484c01cce9e895ecef
Akitosama <138303496+NotWindyZ@users.noreply.github.com>
2026-04-26 14:26:56 +0700

    Added viteSingleFile plugin to frontend vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
 })
